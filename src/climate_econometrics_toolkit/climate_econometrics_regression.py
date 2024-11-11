28f40474e03d1ba5c1a1ce2da1440c10ecec7170
Hayden Freedman <hayden.freedman@gmail.com>
2024-11-11 12:41:06 +0300

    scaling/unscaling bayes regression

--- src/climate_econometrics_toolkit/climate_econometrics_regression.py
+++ src/climate_econometrics_toolkit/climate_econometrics_regression.py
@@ -3,8 +3,11 @@ import pymc as pm
 from pytensor import tensor as pt
 import pickle as pkl
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
 import climate_econometrics_toolkit.climate_econometrics_utils as utils
+
 
 def run_standard_regression(transformed_data, model, demeaned=False):
 	model_vars = utils.get_model_vars(transformed_data, model, demeaned)
@@ -24,27 +27,39 @@ def run_intercept_only_regression(transformed_data, model):
 
 def run_bayesian_regression(transformed_data, model, model_id):
 
-	# TODO: add scaling/unscaling/save to CSV
-
 	model_vars = utils.get_model_vars(transformed_data, model)
+
+	scalers, scaled_data = {}, {}
+	scalers[model.target_var] = StandardScaler()
+	scaled_data[model.target_var] = scalers[model.target_var].fit_transform(np.array(transformed_data[model.target_var]).reshape(-1,1)).flatten()
+	for var in model.covariates:
+		scalers[var] = StandardScaler()
+		scaled_data[var] = scalers[var].fit_transform(np.array(transformed_data[var]).reshape(-1,1)).flatten()
+
+	scaled_df = pd.DataFrame()
+	for var in scaled_data:
+		scaled_df[var] = scaled_data[var]
+	for var in transformed_data:
+		if var not in scaled_df:
+			scaled_df[var] = transformed_data[var]
+
 	print("Fitting Bayesian model containing variables: ", model_vars)
 
 	with pm.Model() as pymc_model:
-		
+
 		covar_coefs = pm.Normal("covar_coefs", 0, 10, shape=(len(model_vars)))
-		covar_terms = pm.Deterministic("regressors", pt.sum(covar_coefs * transformed_data[model_vars], axis=1))
+		covar_terms = pm.Deterministic("regressors", pt.sum(covar_coefs * scaled_df[model_vars], axis=1))
 		intercept = pm.Normal("intercept", 0, 10)
 		target_prior = pm.Deterministic("target_prior", covar_terms + intercept)
 		
 		target_scale = pm.HalfNormal("target_scale", 10)
 		target_std = pm.HalfNormal("target_std", sigma=target_scale)
-		target_posterior = pm.Normal('target_posterior', mu=target_prior, sigma=target_std, observed=transformed_data[model.target_var])
+		target_posterior = pm.Normal('target_posterior', mu=target_prior, sigma=target_std, observed=scaled_df[model.target_var])
 
 		prior = pm.sample_prior_predictive()
 		trace = pm.sample(target_accept=.99, cores=4, tune=1000, draws=1000)
 		posterior = pm.sample_posterior_predictive(trace, extend_inferencedata=True)
 
-	# TODO: save samples as readable CSV
 	with open (f'bayes_samples/bayes_model_{str(model_id)}.pkl', 'wb') as buff:
 		pkl.dump({
 			"prior":prior,
@@ -53,3 +68,8 @@ def run_bayesian_regression(transformed_data, model, model_id):
 			"var_list":model_vars,
 			"target_var":model.target_var
 		},buff)
+
+	unscaled_samples = pd.DataFrame()
+	for index, var in enumerate(model.covariates):
+		unscaled_samples[var] = trace.posterior.covar_coefs[:,:,index].data.flatten() * np.std(transformed_data[model.target_var]) / np.std(transformed_data[var])
+	unscaled_samples.to_csv(f'bayes_samples/coefficient_samples_{str(model_id)}.csv')
